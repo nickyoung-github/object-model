e6812688049c6b1f17781e0d841c7e5fcf3fb91c
nickyoung-github <nick.young.github@gmail.com>
2024-09-08 17:04:36 -0400

    Minor tidying

--- object_model/db/rest.py
+++ object_model/db/rest.py
@@ -1,4 +1,5 @@
 from requests import Session, codes
+from typing import Any
 
 from . import DBContext
 from .persistable import DBRecord
@@ -6,26 +7,23 @@ from ..json import dumps, loads
 
 
 class HttpContext(DBContext):
-    def __init__(self, url: str):
+    def __init__(self, base_url: str):
         super().__init__()
-        self.__url = url
+        self.__base_url = base_url
         self.__session = Session()
 
     def _execute_reads(self, reads: tuple[DBRecord, ...]) -> tuple[DBRecord, ...]:
-        request = {"reads": reads}
-        result = self.__session.post(f"{self.__url}/read", data=dumps(request))
-        if result.status_code == codes.ok:
-            return tuple(DBRecord(**r) for r in loads(result.content))
-        else:
-            result.raise_for_status()
+        return self.__post("read", {"reads": reads})
 
     def _execute_writes(self,
                         writes: tuple[DBRecord, ...],
                         username: str,
                         hostname: str,
                         comment: str) -> tuple[DBRecord, ...]:
-        request = {"writes": writes, "username": username, "hostname": hostname, "comment": ""}
-        result = self.__session.post(f"{self.__url}/write", data=dumps(request))
+        return self.__post("write", {"writes": writes, "username": username, "hostname": hostname, "comment": ""})
+
+    def __post(self, endpoint: str, request: dict[str, Any]):
+        result = self.__session.post(f"{self.__base_url}/{endpoint}", data=dumps(request))
         if result.status_code == codes.ok:
             return tuple(DBRecord(**r) for r in loads(result.content))
         else:
