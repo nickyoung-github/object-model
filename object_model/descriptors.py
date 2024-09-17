8763fcc311515da993d2c07e92c0e94e07524d5d
nickyoung-github <nick.young.github@gmail.com>
2024-09-17 09:58:19 -0400

    Remove value descriptor

--- object_model/descriptors.py
+++ object_model/descriptors.py
@@ -17,16 +17,3 @@ class Id:
     def __set_name__(self, owner, name):
         if not self.__type:
             self.__type = owner
-
-
-class Value:
-    def __init__(self, value):
-        self.__value = value
-
-    def __get__(self, instance, owner):
-        if instance is not None:
-            return instance.__value or self.__value
-
-    def __set__(self, instance, value):
-        instance.__value = value
-        return instance
