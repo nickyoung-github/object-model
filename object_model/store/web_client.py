f584d7bf5fb3cfa46250ac8a20e5800b7bba61a6
nickyoung-github <nick.young.github@gmail.com>
2024-11-19 16:46:16 -0500

    Fix web_client

--- object_model/store/web_client.py
+++ object_model/store/web_client.py
@@ -1,33 +1,28 @@
+from pydantic import BaseModel
 from requests import Session, codes
-from typing import Any
 
-from .._json import dumps, loads, schema
+from .._json import dumps, loads
 
 from .object_record import ObjectRecord
-from .persistable import PersistableMixin
-from .object_store import ObjectStore
+from .object_store import ObjectStore, ReadRequest, RegisterSchemaRequest, WriteRequest
 
 
 class WebStoreClient(ObjectStore):
     def __init__(self, base_url: str):
-        super().__init__(False)
+        super().__init__(False, False)
         self.__base_url = base_url
         self.__session = Session()
 
-    def _execute_reads(self, reads: tuple[ObjectRecord, ...]) -> tuple[ObjectRecord, ...]:
-        return self.__post("read", {"reads": reads})
+    def _execute_reads(self, reads: ReadRequest) -> tuple[ObjectRecord, ...]:
+        return self.__post("read", reads)
 
-    def _execute_writes(self,
-                        writes: tuple[ObjectRecord, ...],
-                        username: str,
-                        hostname: str,
-                        comment: str) -> tuple[ObjectRecord, ...]:
-        return self.__post("write", {"writes": writes, "username": username, "hostname": hostname, "comment": ""})
+    def _execute_writes(self, writes: WriteRequest) -> tuple[ObjectRecord, ...]:
+        return self.__post("write", writes)
 
-    def register_type(self, typ: type[PersistableMixin]):
-        self.__post("register_type", {"name": typ.object_type, "schema": schema(typ)})
+    def register_schema(self, json_schema: RegisterSchemaRequest):
+        self.__post("register", json_schema)
 
-    def __post(self, endpoint: str, request: dict[str, Any]):
+    def __post(self, endpoint: str, request: BaseModel):
         result = self.__session.post(f"{self.__base_url}/{endpoint}", data=dumps(request))
         if result.status_code == codes.ok:
             return tuple(ObjectRecord(**r) for r in loads(result.content))
